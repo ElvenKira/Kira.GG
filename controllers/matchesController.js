0f15d0ef34e7f30b7824500a866794a83530ab72
Leonardo Avila <templar.of.eva@gmail.com>
2018-03-24 23:41:34 -0300

    test deploy

--- controllers/matchesController.js
+++ controllers/matchesController.js
@@ -4,12 +4,38 @@ var config = require('../config');
 exports.matches_get_data_by_name = function(req, res, next) {
     //var summoner = req.params.summoner_name;
     var summoner = req.body.summoner_name;
-    var lane = req.body.lane;
-    var role = req.body.role;
-    var matches = req.body.matches;
+    var lane = "";
+    var role = "";
+
+    switch(req.body.role) {
+        case "ADC":
+            lane = "BOTTOM";
+            role = "DUO_CARRY";
+            break;
+        case "Support":
+            lane = "BOTTOM";
+            role = "DUO_SUPPORT";
+            break;
+        case "Mid":
+            lane = "MID";
+            role = "SOLO";
+            break;
+        case "Top":
+            lane = "TOP";
+            role = "SOLO";
+            break;
+        case "Jungle":
+            lane = "JUNGLE";
+            role = "SOLO";
+            break;
+        case "Any":
+            lane = "ANY";
+            role = "ANY";
+            break;
+    }
     
-    console.log(summoner);
-    console.log("test: " + config.URL_SUMMONER_BY_NAME + summoner);
+    var matches = req.body.matches;
+    var date = new Date(req.body.date).getTime();
     var options = {
         url: config.URL_SUMMONER_BY_NAME + summoner, 
         headers: {
@@ -31,18 +57,24 @@ exports.matches_get_data_by_name = function(req, res, next) {
         request(options, function(error_matches, response_matches, data_matches) {
             data.matches = JSON.parse(data_matches);
             var match_count = parseInt(matches);
-            var count = 0;
-            var count_inside = 0;
+            var count = data.matches.matches.length;
 
             console.log("\n\nMatches: " + data.matches.matches.length);
             console.log("Match Count: " + match_count + "\n\n");
 
 
             // Iterate over each match and look for the match info 
-            for (var i = 0; i < data.matches.matches.length && count < match_count; i++) {
-                if ( data.matches.matches[i].lane.localeCompare(lane) == 0 &&
-                data.matches.matches[i].queue == 420) {
+            for (var i = 0; i < data.matches.matches.length; i++) {
 
+                // Check that Role matches
+                if ( 
+                    (lane == "ANY" || 
+                    (
+                        data.matches.matches[i].lane.localeCompare(lane) == 0 &&
+                        data.matches.matches[i].role.localeCompare(role) == 0
+                    )
+                    ) && data.matches.matches[i].queue == 420
+                ) {
                     var match_detail_option = {
                         url: config.URL_MATCH_BY_ID, 
                         headers: {
@@ -50,22 +82,23 @@ exports.matches_get_data_by_name = function(req, res, next) {
                         }
                     }
 
-                    count++;
                     match_detail_option.url = match_detail_option.url.replace( "{matchId}", data.matches.matches[i].gameId);
-                    console.log(count + ": " + data.matches.matches[i].gameId);
 
                     get_match_detail(match_detail_option, function(err, match) {
+                        
                         if (err) {
                             console.log("Error: " + String(err));
                         } else {
                             data.match_detail[match.gameCreation] = match;
-                            console.log("Added already: " + Object.keys(data.match_detail).length + " - gameCreation : " + match.gameCreation);
-                            //if (++count_inside == match_count) {
-                            if (Object.keys(data.match_detail).length == match_count) {
+
+                            --count;
+                            if (Object.keys(data.match_detail).length == match_count || count == 0) {
                                 finish_getting_data(); 
                             }
                         }
                     })
+                } else {
+                    --count;
                 }
             }
 
@@ -75,14 +108,15 @@ exports.matches_get_data_by_name = function(req, res, next) {
                     
                     if (match_obj.status !== undefined) {
                         setTimeout(function() {
-                            console.log("retrying for " + Object.keys(data.match_detail).length);
-                            console.log("Message: "+ JSON.stringify(match_obj));
-                            console.log("URL: " + match_detail_option.url);
                             get_match_detail(match_detail_option, callback);
                         }, 1500)
-                    } else {
-                        console.log("on Else: " + match_obj.gameCreation);
+                    } else if (match_obj.gameCreation >= date) {
                         callback(null, match_obj);
+                    } else { 
+                        --count;
+                        if (Object.keys(data.match_detail).length == match_count || count == 0) {
+                            finish_getting_data(); 
+                        }
                     }
                 });
             }
@@ -91,15 +125,14 @@ exports.matches_get_data_by_name = function(req, res, next) {
                 req.app.db.collection('champions').findOne({}, function(err, data_champions) {
                     if (err) { 
                         console.log(String(err));
-                    } else {
-                        console.log("champions: \n" + JSON.stringify(data_champions));
                     }
 
                     res.render('matches_data', 
                         {
                             'data': data,
                             'champions' : data_champions,
-                            'matches': match_count
+                            'matches': match_count,
+                            'date_in_milliseconds': date
                         });
                 });
             }

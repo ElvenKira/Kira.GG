a96a2c1138041b078554ce7e4d075240c89dfe31
Leonardo Avila <templar.of.eva@gmail.com>
2018-03-27 01:43:38 -0300

    change the way the program works. it now adds them synchronous

--- controllers/matchesController.js
+++ controllers/matchesController.js
@@ -36,9 +36,13 @@ exports.matches_get_data_by_name = function(req, res, next) {
             break;
     }
     
-    var matches = req.body.matches;
+    var matches = 10;
+    if (!isNaN(req.body.matches)) {
+        matches  = req.body.matches;
+    }
+
     var from_date = new Date(req.body.from_date).getTime();
-    var to_date = new Date(req.body.to_date).getTime();
+    var to_date =   new Date(req.body.to_date).getTime();
 
     var options = {
         url: config.URL_SUMMONER_BY_NAME + summoner, 
@@ -49,20 +53,37 @@ exports.matches_get_data_by_name = function(req, res, next) {
 
     var data = {};
     data.match_detail = {};
+    var output_records = 0;
 
     // Get Summoner Info
-    request(options, function(error_summoner, response_summoner, data_summoner) {
+    request(options, function(error_summoner, response_summoner, data_summoner) {        
         data.summoner = JSON.parse(data_summoner);
+
+        if (data.summoner.status !== undefined && data.summoner.status.message == "Forbidden") {
+            console.log("Forbidden!");
+            res.redirect("/");
+        }
         
         options.url = config.URL_100_MATCHES_BY_ID;
         options.url = options.url.replace( "{accountId}", data.summoner.accountId);
-        options.url += "&endTime=" + to_date + "&beginTime=" + from_date + "&queue=420";
+        if (!isNaN(to_date)) {
+            options.url += "&endTime=" + to_date;
+        }
+        if (!isNaN(from_date)) {
+            options.url += "&beginTime=" + from_date;
+        }
+        options.url += "&queue=420";
+
         console.log(options.url);
         
         // Get latest Ranked matches
         request(options, function(error_matches, response_matches, data_matches) {
             data.matches = JSON.parse(data_matches);
-            
+            if (data.matches.status !== undefined && data.matches.status.message == "Forbidden") {
+                console.log("Forbidden!");
+                res.redirect("/");
+            }
+
             if (data.matches.matches === undefined ) {
                 console.log(JSON.stringify(data.matches));
                 finish_getting_data();
@@ -72,85 +93,69 @@ exports.matches_get_data_by_name = function(req, res, next) {
     
                 console.log("\n\nMatches: " + data.matches.matches.length);
                 console.log("Match Count: " + match_count + "\n\n");
-    
-    
-                // Iterate over each match and look for the match info 
-                for (var i = 0; i < data.matches.matches.length; i++) 
-                {
-                    if (lane == "ANY" || (data.matches.matches[i].lane.localeCompare(lane) == 0 && data.matches.matches[i].role.localeCompare(role) == 0)) 
-                    {
-                        // Get Match Detail
-                        get_match_detail(data.matches.matches[i].gameId, function(err, match) {
-                            if (err) {
-                                console.log("Error: " + String(err));
-                            } else {
-                                data.match_detail[match.gameCreation] = match;
-    
-                                if (--count == 0) {
-                                    finish_getting_data(); 
-                                } 
-                            }
+
+                process_data(data.matches.matches, 1);
+
+                
+                function process_data(match_data, count) {
+                    if (output_records >= match_count || count >= match_data.length) {
+                        req.app.db.collection('champions').findOne({}, function(err, data_champions) {
+                            res.render('matches_data', 
+                            {
+                                'data': data,
+                                'champions' : data_champions,
+                                'matches': match_count
+                            });
                         });
-
-                    } else if (--count == 0) {
-                        finish_getting_data(); 
-                    }
-                }
-            }
-
-            // Return match info after requesting it.
-            function get_match_detail(matchId, callback) {
-
-                // TODO > check if match id is on the database, else query for it
-                var match_detail_option = 
-                {
-                    url: config.URL_MATCH_BY_ID, 
-                    headers: {
-                        "X-Riot-Token": RIOT_KEY
-                    }
-                }
-                match_detail_option.url = match_detail_option.url.replace( "{matchId}", matchId);
-
-                // Request timeline info
-                request(match_detail_option, function(error_match, response_match, data_match) {
-                    // Parse Timeline
-                    var match_obj = JSON.parse(data_match);
-
-                    // If timeline was not returned ok try again 1.5 seconds later
-                    if (match_obj.status !== undefined) {
-                        setTimeout(function() {
-                            get_match_detail(matchId, callback);
-                        }, 1500)
-                    
-                    // If timeline was return and gameCreation date is inside the time frame
-                    } else if (match_obj.gameCreation >= from_date || match_obj.gameCreation <= to_date ) {
-                        callback(null, match_obj);
-
-                    // If not in the timeframe
                     } else {
-                        if (--count == 0) {
-                            finish_getting_data(); 
+                        if (lane == "ANY" || (data.matches.matches[count - 1].lane.localeCompare(lane) == 0 && data.matches.matches[count - 1].role.localeCompare(role) == 0)) 
+                        {
+                            // TODO > check if match id is on the database, else query for it
+                            var match_detail_option = { url: config.URL_MATCH_BY_ID, headers: {"X-Riot-Token": RIOT_KEY}};
+                            match_detail_option.url = match_detail_option.url.replace( "{matchId}", data.matches.matches[count - 1].gameId);
+    
+                            // Request timeline info
+                            request(match_detail_option, function(error_match, response_match, data_match) {
+                                // Parse Timeline
+                                var match_obj = JSON.parse(data_match);
+                                if (match_obj.status !== undefined && match_obj.status.message == "Forbidden") {
+                                    data.match_detail[match_obj.gameCreation] = match_obj;
+                                    console.log("Retrying for " + data.matches.matches[count - 1].gameId + "!");
+                                    setTimeout(function() {
+                                        process_data(match_data, count);
+                                    }, 1200);
+                                } else {
+                                    // Add to data output
+                                    data.match_detail[match_obj.gameCreation] = match_obj;
+                                    output_records++;
+
+                                    var limitCount = response_match["headers"]["x-app-rate-limit-count"].split(",");
+                                    if (output_records < match_count && match_data.length > count) {
+                                        if (limitCount[0] == "20:1") {
+                                            setTimeout(function() {
+                                                process_data(match_data, ++count);
+                                            }, 1200);
+                                        } else {
+                                            process_data(match_data, ++count);
+                                        }
+                                    } else {
+                                        req.app.db.collection('champions').findOne({}, function(err, data_champions) {
+                                            res.render('matches_data', 
+                                            {
+                                                'data': data,
+                                                'champions' : data_champions,
+                                                'matches': match_count
+                                            });
+                                        });
+                                    }
+                                }
+                            });
+                        } else {
+                            process_data(match_data, ++count);
                         }
                     }
-                });
+                }
             }
-
-            function finish_getting_data() {
-                req.app.db.collection('champions').findOne({}, function(err, data_champions) {
-                    if (err) { 
-                        console.log(String(err));
-                    }
-
-                    res.render('matches_data', 
-                        {
-                            'data': data,
-                            'champions' : data_champions,
-                            'matches': match_count,
-                            'date_in_milliseconds': from_date
-                        });
-                });
-            }
-
         });
     });
 }
